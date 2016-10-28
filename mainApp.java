be4a47e4d6ba0f242fd520387b9d1b9acfa6605d
Michael Bottini <mbottini1@gmail.com>
2016-10-27 19:50:28 -0700

    Created Database.removePatient(ID).
    * Boolean function. Sets enrollmentStatus flag to 0 on the specified ID.
    
    * Returns false if the ID doesn't exist, returns true if it finds a row with
      that ID.

--- mainApp.java
+++ mainApp.java
@@ -14,19 +14,26 @@ public class mainApp
             System.out.println("Updating patient...");
 
             try {
-            Patient updatePatient = new Patient("Michael Bottini", "4 Cross St",
-                    "Southborough", "MA", "91234", 1, 0);
-            if(dBase.updatePatient(5912, updatePatient)) {
-                System.out.println("Patient updated.");
+                Patient updatePatient = new Patient("Michael Bottini", 
+                        "4 Cross St", "Southborough", "MA", "91234", 1, 0);
+                if(dBase.updatePatient(5912, updatePatient)) {
+                    System.out.println("Patient updated.");
+                }
+                else {
+                    System.out.println("ID not found.");
+                }
+            }
+            catch(InputException e) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.println("\nRemoving ID = 1...");
+            if (dBase.removePatient(1)) {
+                System.out.println("Removed.");
             }
 
             else {
-                System.out.println("ID not found.");
-            }
-            }
-
-            catch(InputException e) {
-                System.out.println(e.getMessage());
+                System.out.println("Not found.");
             }
 
             System.out.println();
